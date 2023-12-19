db388b29485619048281e2f79a1ee2738d9a1b79
Brandon [White Fox] <r.rakeda@gmail.com>
2023-12-19 18:54:36 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ mongod --fork --logpath /var/log/mongod.log --bind_ip_all
 # Restore from dump
 mongorestore /data/dump/
 
-# Now, since MongoDB is already running in the background, 
-# we don't need to start it again. Instead, we wait for the background MongoDB process to complete.
-wait
+# Stop the background MongoDB
+mongod --shutdown
+
+# Start MongoDB in the foreground
+exec mongod --bind_ip_all
